bedf1727b5377a265da4e09470a7a260bc647c01
Juan Martin Gimenez <jm.gimenez.inf@gmail.com>
2017-03-15 12:19:51 -0300

    funciona como servidor stático.. Con react build se construye los archivos staticos

--- cefi-copias/server/server.babel.js
+++ cefi-copias/server/server.babel.js
@@ -1,45 +1,10 @@
 import express from 'express';
-//import mysql from 'mysql';
+import React from 'react';
 
 const app = express();
 
-app.use('/', express.static('public'));
+app.use(express.static('../build'));
 
-//app.listen(process.env.PORT || 3000);
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.use('/', express.static('public'));
 app.listen(3001, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Servidor statico Express-React port 3001!');
 });
-//crea conexion mysql
-// var connection = mysql.createConnection({
-//    host     : 'localhost',
-//    user     : 'root',
-//    password : 'root',
-//    database : 'mydb'
-//  });
-
- //conecta con mysql
-//  connection.connect(function(err){
-//  if(!err) {
-//      console.log("Database is connected ... \n\n");  
-//  } else {
-//      console.log("Error connecting database ... \n\n");  
-//  }
-//  });
- 
- //test de conexion 
-//  app.get("/",function(req,res){
-//  connection.query('SELECT * from Pedido', function(err, rows, fields) {
-//  connection.end();
-//    if (!err)
-//      console.log('El resultado es: ', rows);
-//    else
-//      console.log('Error while performing Query.');
-//    });
-//  });
- 
- // run puerto
-
