52c48eb88e0aed91e7e83bb675242b47b80c6c86
Juan Martin Gimenez <jm.gimenez.inf@gmail.com>
2017-04-11 15:08:01 -0300

    primera version de busqueda con MySQL

--- cefi-copias/src/componentes/js/Buscador.js
+++ cefi-copias/src/componentes/js/Buscador.js
@@ -1,33 +1,62 @@
 import React, { Component } from 'react';
-import {Row,Col,Input,Icon} from 'react-materialize';
+import {Col,Input,Icon} from 'react-materialize';
 import '../css/buscador.css'
+import axios from 'axios';
+
+function ResultadoList(result) {
+  const listItems = result.map((number) =>
+    <li>
+      {number.etiqueta}
+    </li>
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
 class Buscador extends Component {
+
  constructor(props) {
     super(props);
     this.handleSearch = this.handleSearch.bind(this);
-    this.state={texto:""}
+    this.state={resultado:""};
   }
 handleSearch(e){
- console.log(e.target.value);
-     this.setState({
-      texto:e.target.value
-    });
- e.preventDefault();
+    console.log(e.target.value);
+    var buscado=e.target.value;
+    var self=this;
+    if (buscado == ""){
+       self.setState({
+            resultado:""
+        });
+    }else{
+      axios.get('http://localhost:3001/tags')
+      .then(function(response){
+            console.log(response.data); 
+            console.log(response.status); 
+            var result=response.data.tags;
+            self.setState({
+            resultado: ResultadoList(result)
+          });
+      });
+    }
+
+ 
 }
   render() {
     return (
+      
     <div className="StyleBuscador">
             <Col l={12} m={12} s={12}>
                <Input type="text" s={12} label="" validate onChange={(e)=> this.handleSearch(e)}><Icon>search</Icon></Input>
             </Col>
-            
-                    <Col className="z-depth-2" l={12} m={12} s={12}>
-                    <div className="StyleResultado">
-                      <a href=""><h6>{this.state.texto}</h6></a>
-                      <a href=""><h6>{this.state.texto}</h6></a>
-                    </div>
-                    </Col>
-            
+            <Col className="z-depth-2" l={12} m={12} s={12}>
+                <div className="StyleResultado">
+                  {this.state.resultado}
+                </div>
+            </Col>
     </div>
     
     );
